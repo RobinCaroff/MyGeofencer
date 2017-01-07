7adef7d5fc2ba1679143944d42d82f87405ea76c
Robin Caroff <robin.caroff@gmail.com>
2017-01-07 20:10:11 +0100

    Adds the a model class to hold geofence data. Adds the Gson library.

---
+++ app/src/main/java/com/robincaroff/mygeofencer/models/MyGeofence.java
@@ -0,0 +1,13 @@
+package com.robincaroff.mygeofencer.models;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Model used to hold geofence data.
+ */
+
+
+public class MyGeofence {
+    private LatLng location;
+    private String name;
+}
